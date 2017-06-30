b0062d15ef795c77f14a53c1a67c76fb9ccd0b15
Kjartan Johansen <kjartan@artofwar.cc>
2017-07-01 08:29:24 +1000

    Added PersistenceConstants to cover read and write constants

---
+++ src/Circle314/Concept/Persistence/PersistenceConstants.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Circle314\Concept\Persistence;
+
+abstract class PersistenceConstants
+{
+    const READ = 'READ';
+    const WRITE = 'WRITE';
+
+    final private function __construct()
+    {
+
+    }
+}
