b665ce57f488d1832624735bee903f3e29029135
Kjartan Johansen <kjartan@artofwar.cc>
2018-01-26 09:19:47 +1100

    Added a missing @deprecated tag to DataMediatorInterface. Updated composer.json to stipulate the licence as Apache-2.0 instead of just Apache.

--- src/Component/Data/Mediator/DataMediatorInterface.php
+++ src/Component/Data/Mediator/DataMediatorInterface.php
@@ -7,6 +7,7 @@ use Circle314\Concept\Persistence\PersistenceInterface;
 /**
  * Interface DALInterface
  * @package Circle314\Component\Data
+ * @deprecated 0.6
  * @see \Circle314\Component\Data\Persistence\Strategy\PersistenceStrategyInterface;
  */
 interface DataMediatorInterface extends PersistenceInterface
